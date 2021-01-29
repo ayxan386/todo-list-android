2e5a9b8ec811030a399d89faf1862b6d8b1f2371
ayxan386 <ayxanpro@gmail.com>
2021-01-29 22:05:18 +0400

    Added login layout

---
+++ app/src/main/java/com/jsimplec/todolist/activity/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.jsimplec.todolist.activity.auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jsimplec.todolist.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
