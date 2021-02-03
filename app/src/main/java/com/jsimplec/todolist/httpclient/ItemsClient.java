9ad86b520b4f29b5fdf7583c2de488398ebb954e
ayxan386 <ayxanpro@gmail.com>
2021-02-03 22:15:52 +0400

    Implemented update item http request

--- app/src/main/java/com/jsimplec/todolist/httpclient/ItemsClient.java
+++ app/src/main/java/com/jsimplec/todolist/httpclient/ItemsClient.java
@@ -1,8 +1,12 @@
 package com.jsimplec.todolist.httpclient;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.jsimplec.todolist.activity.main.MainActivity;
 import com.jsimplec.todolist.callback.SuccessErrorCallBack;
+import com.jsimplec.todolist.model.Item;
 import com.jsimplec.todolist.model.ItemList;
 import com.jsimplec.todolist.model.RestResponseDto;
 import com.jsimplec.todolist.util.HttpUtils;
@@ -11,15 +15,18 @@ import com.jsimplec.todolist.util.constants.StaticConstants;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import static com.jsimplec.todolist.util.HttpUtils.REST_RESPONSE_ITEM_TYPE;
+import static com.jsimplec.todolist.util.HttpUtils.REST_RESPONSE_LIST_ITEMLIST_TYPE;
+import static com.jsimplec.todolist.util.constants.StaticConstants.JSON;
 import static com.jsimplec.todolist.util.constants.StaticConstants.MS_BASE_URL;
 
 public class ItemsClient {
@@ -49,11 +56,40 @@ public class ItemsClient {
                     @Override
                     public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                         String responseBody = response.body().string();
-                        Type collectionType = new TypeToken<RestResponseDto<List<ItemList>>>() {
-                        }.getType();
-                        RestResponseDto<List<ItemList>> responseDto = objectMapper.fromJson(responseBody, collectionType);
+
+                        RestResponseDto<List<ItemList>> responseDto = objectMapper.fromJson(responseBody, REST_RESPONSE_LIST_ITEMLIST_TYPE);
                         if (responseDto.getMessage().equals(REST_SUCCESS_MESSAGE)) {
                             callBack.onSuccess(responseDto.getData());
+                        } else {
+                            callBack.onError(responseDto.getMessage());
+                        }
+                    }
+                });
+    }
+
+    public void updateItem(Item item, SuccessErrorCallBack<String> callBack) {
+        SharedPreferences preferences = MainActivity.applicationContext
+                .getSharedPreferences(StaticConstants.PREFERENCE_TODO_AUTH, Context.MODE_PRIVATE);
+        String token = preferences.getString("token", "123");
+        String requestBody = objectMapper.toJson(item);
+        Request request = new Request.Builder()
+                .url(String.format("%s/item-list/update-item", MS_BASE_URL))
+                .header(StaticConstants.AUTHORIZATION_HEADER_NAME, HttpUtils.makeAuthHeader(token))
+                .put(RequestBody.create(requestBody, JSON))
+                .build();
+
+        httpClient.newCall(request)
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        callBack.onError(e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        RestResponseDto<Item> responseDto = objectMapper.fromJson(response.body().string(), REST_RESPONSE_ITEM_TYPE);
+                        if (responseDto.getMessage().equals(REST_SUCCESS_MESSAGE)) {
+                            callBack.onSuccess("success");
                         } else {
                             callBack.onError(responseDto.getMessage());
                         }
