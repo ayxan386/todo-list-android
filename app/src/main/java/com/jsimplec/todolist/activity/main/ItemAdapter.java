4e0a0465fe50727ce574e5e1def9722290b724c0
ayxan386 <ayxanpro@gmail.com>
2021-02-03 22:15:31 +0400

    Added action listener for item check box

--- app/src/main/java/com/jsimplec/todolist/activity/main/ItemAdapter.java
+++ app/src/main/java/com/jsimplec/todolist/activity/main/ItemAdapter.java
@@ -1,8 +1,10 @@
 package com.jsimplec.todolist.activity.main;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,8 +12,13 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.checkbox.MaterialCheckBox;
 import com.jsimplec.todolist.R;
+import com.jsimplec.todolist.callback.ErrorCallBack;
+import com.jsimplec.todolist.httpclient.ItemsClient;
 import com.jsimplec.todolist.model.Item;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ItemAdapter extends ListAdapter<Item, ItemAdapter.ViewHolder> {
 
@@ -35,14 +42,31 @@ public class ItemAdapter extends ListAdapter<Item, ItemAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         private final TextView title;
+        private final MaterialCheckBox status;
+        private Item item;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.itemTitle);
+            status = itemView.findViewById(R.id.itemCheckBox);
+
+            status.setOnCheckedChangeListener(updateStatus());
+        }
+
+        @NotNull
+        private CompoundButton.OnCheckedChangeListener updateStatus() {
+            ErrorCallBack callback = errorMessage -> Log.e("ItemAdapter http call", errorMessage);
+            return (buttonView, isChecked) ->
+                    ItemsClient.ITEMS_CLIENT
+                            .updateItem(
+                                    item.buildItem(item.getContent(), Item.getStatusFromBool(isChecked)),
+                                    callback);
         }
 
         public void bind(Item item) {
             title.setText(item.getTitle());
+            status.setActivated(Item.getActiveStatus(item.getStatus()));
+            this.item = item;
         }
     }
 
