bbd5e3ebaf12269d667523637c9569b8a5ad2de3
ayxan386 <ayxanpro@gmail.com>
2021-02-04 22:11:03 +0400

    Added item detail view

---
+++ app/src/main/java/com/jsimplec/todolist/activity/main/ItemDetailActivity.java
@@ -0,0 +1,18 @@
+package com.jsimplec.todolist.activity.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jsimplec.todolist.R;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_item_detail);
+    }
+}
