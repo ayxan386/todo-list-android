31a08003f089b87d5ef40fd963e2b809f2778827
ayxan386 <ayxanpro@gmail.com>
2021-02-05 21:22:55 +0400

    Implemented onClick action listener

--- app/src/main/java/com/jsimplec/todolist/activity/main/ItemDetailActivity.java
+++ app/src/main/java/com/jsimplec/todolist/activity/main/ItemDetailActivity.java
@@ -1,13 +1,20 @@
 package com.jsimplec.todolist.activity.main;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 import com.jsimplec.todolist.R;
+import com.jsimplec.todolist.callback.ErrorCallBack;
+import com.jsimplec.todolist.httpclient.ItemsClient;
 import com.jsimplec.todolist.model.Item;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
@@ -17,15 +24,37 @@ public class ItemDetailActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_item_detail);
         Item item = getIntent().getExtras().getParcelable("item");
-        Log.i("Item detail", item.toString());
 
         TextView title = findViewById(R.id.detailTitle);
-        TextView content = findViewById(R.id.detailContent);
+        TextInputLayout content = findViewById(R.id.detailContent);
         TextView date = findViewById(R.id.updateDate);
 
         title.setText(item.getTitle());
-        content.setText(item.getContent());
+        content.getEditText().setText(item.getContent());
         date.setText(item.getUpdateDate());
 
+        Button submitButton = findViewById(R.id.detailSubmit);
+        submitButton.setOnClickListener(updateItemRequest(item, content));
+
+    }
+
+    @NotNull
+    private View.OnClickListener updateItemRequest(Item item, TextInputLayout content) {
+        return v -> {
+            Item updatedItem = item.buildItem(content.getEditText().getText().toString(), item.getStatus());
+            ItemsClient.ITEMS_CLIENT.updateItem(updatedItem, (ErrorCallBack) errorMessage -> {
+                runOnUiThread(() -> displayErrorSnackBar(errorMessage));
+            });
+        };
+    }
+
+    private void displayErrorSnackBar(String errorMessage) {
+        View rootView = findViewById(android.R.id.content);
+        Snackbar snackbar = Snackbar.make(rootView, errorMessage, Snackbar.LENGTH_INDEFINITE);
+        snackbar.setActionTextColor(getResources().getColor(R.color.purple_500));
+        snackbar.setAction("Close", v -> {
+            snackbar.dismiss();
+        });
+        snackbar.show();
     }
 }
